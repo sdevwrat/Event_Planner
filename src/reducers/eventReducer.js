71613f425e178e09cea8cbcd2891fcb1e158fe88
Devwrat Sahu <sdevwrat@gmail.com>
2020-12-08 22:23:36 +0530

    default events

--- src/reducers/eventReducer.js
+++ src/reducers/eventReducer.js
@@ -1,10 +1,10 @@
 const initState = {
     events: [
-        {id:1,title:"hello Bi", desc: "Lorem ipsum dolor",startDate:"2020-12-10",stopDate:"2020-12-16",people:'Frank',category:'Blog Infographic'},
-        {id:2,title:'heyy title', desc: "Lorem ipsum dolor",startDate:"2020-12-09",stopDate:"2020-12-13",people:'Keshav',category:'Nike Project'},
-        {id:3,title:'qwrty', desc: "Lorem ipsum dolor",startDate:"2020-12-16",stopDate:"2020-12-22",people:'Devu',category:'Envato Meetups'},
-        {id:4,title:'Winter body', desc: "Lorem ipsum dolor",startDate:"2020-10-01",stopDate:"2020-11-01",people:'Shivam',category:'Nike Project'},
-        {id:5,title:'Winter jacket', desc: "Lorem ipsum dolor",startDate:"2020-12-05",stopDate:"2021-12-06",people:'Shivam',category:'Blog Infographic'},
+        {id:1,title:"Design Form Infographic", desc: "Lorem ipsum dolor",startDate:"2020-12-10",stopDate:"2020-12-16",people:'Frank',category:'Blog Infographic'},
+        {id:2,title:'Nike Case Study', desc: "Lorem ipsum dolor",startDate:"2020-12-09",stopDate:"2020-12-13",people:'Keshav',category:'Nike Project'},
+        {id:3,title:'Layout Design', desc: "Lorem ipsum dolor",startDate:"2020-12-16",stopDate:"2020-12-22",people:'Devu',category:'Envato Meetups'},
+        {id:4,title:'hello title', desc: "Lorem ipsum dolor",startDate:"2020-10-01",stopDate:"2020-11-01",people:'Shivam',category:'Nike Project'},
+        {id:5,title:'qwerty', desc: "Lorem ipsum dolor",startDate:"2020-12-05",stopDate:"2021-12-06",people:'Shivam',category:'Blog Infographic'},
     ]
 }
 const eventReducer= (state = initState,action)=>{
